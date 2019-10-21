3e459f57dfaf5c4f547b2f51f08ee55f1a84eebf
bajinho <bajo@live.jp>
2019-10-21 11:25:51 -0300

    Fixed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import './pages/firstpage.dart' show FirstPage;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+void main() => runApp(MaterialApp(
+      title: 'Navigation Basics',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: FirstPage(),
-    );
-  }
-}
+    ));
+
